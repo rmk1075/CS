0f0129700d38985afb53b2aa43f9c57cee38a136
jamie <rmk1075@gmail.com>
2021-06-03 01:46:41 +0900

    feat: add QuickSort and test

--- Algorithm/Test/SortTest.java
+++ Algorithm/Test/SortTest.java
@@ -8,10 +8,23 @@ import Algorithm.Sorting.BubbleSort.BubbleSort;
 import Algorithm.Sorting.HeapSort.HeapSort;
 import Algorithm.Sorting.InsertionSort.InsertionSort;
 import Algorithm.Sorting.MergeSort.MergeSort;
+import Algorithm.Sorting.QuickSort.QuickSort;
 import Algorithm.Sorting.SelectionSort.SelectionSort;
 
 public class SortTest {
     public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            try {
+                test();
+            } catch(Exception e) {
+                System.out.println(e);
+                break;
+            }
+
+        }
+    }
+
+    public static void test() {
         int N = 10;
         int[] arr = new int[N];
         Random rand = new Random();
@@ -42,6 +55,10 @@ public class SortTest {
 
         sort = new HeapSort(arr);
         System.out.print("HeapSort:\t");
+        System.out.println(Arrays.toString(sort.sort()));
+
+        sort = new QuickSort(arr);
+        System.out.print("QuickSort:\t");
         System.out.println(Arrays.toString(sort.sort()));
     }
 }
