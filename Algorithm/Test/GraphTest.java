8aa6a68986e8ad313fc4884eb6ace540516f032b
jamie <rmk1075@gmail.com>
2021-06-05 11:24:11 +0900

    feat: add Dijkstra and test

--- Algorithm/Test/GraphTest.java
+++ Algorithm/Test/GraphTest.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import Algorithm.Graph.Graph;
 import Algorithm.Graph.BFS.BFS;
 import Algorithm.Graph.DFS.DFS;
+import Algorithm.Graph.Dijkstra.Dijkstra;
 
 public class GraphTest {
     public static void main(String[] args) {
@@ -25,9 +26,11 @@ public class GraphTest {
             }
         }
 
+        System.out.println("non weighted graph");
         for(int i = 0; i < N; i++) {
             System.out.println(Arrays.toString(map[i]));
         }
+        System.out.println();
         
         Graph graph;
         int[] result;
@@ -37,6 +40,25 @@ public class GraphTest {
         System.out.println(Arrays.toString(result));
 
         graph = new BFS(map);
+        result = graph.search();
+        System.out.println(Arrays.toString(result));
+
+
+        int[][] weightedMap = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = i + 1; j < N; j++) {
+                weightedMap[i][j] = weightedMap[j][i] = rand.nextInt(100);
+            }
+        }
+
+        System.out.println("-----------------------------------");
+        System.out.println("weighted graph");
+        for(int i = 0; i < N; i++) {
+            System.out.println(Arrays.toString(weightedMap[i]));
+        }
+        System.out.println();
+
+        graph = new Dijkstra(weightedMap);
         result = graph.search();
         System.out.println(Arrays.toString(result));
     }
