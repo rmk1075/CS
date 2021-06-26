864d5e1a55a5e1db0a4a35f924ae2bc88222386a
jamie <rmk1075@gmail.com>
2021-06-26 12:43:21 +0900

    feat: merge sort 주석 추가

--- Algorithm/Sorting/MergeSort/MergeSort.java
+++ Algorithm/Sorting/MergeSort/MergeSort.java
@@ -22,8 +22,11 @@ public class MergeSort implements Sort {
             return ;
         }
 
+        //  배열을 2 부분으로 나눈다.
         divide(arr, l, (l + r) / 2);
         divide(arr, (l + r) / 2, r);
+
+        // 두 부분 배열을 병합한다.
         merge(arr, l, (l + r) / 2, r);
     }
 
@@ -31,6 +34,8 @@ public class MergeSort implements Sort {
         int N = r - l, left = l;
         int[] temp = new int[N];
 
+        // 두 부분 배열을 병합한다.
+        // 이때 정렬을 진행하여 임시 배열 temp에 저장한다.
         int index = 0;
         for(int i = m; i < r; i++) {
             if(l == m) {
@@ -50,6 +55,7 @@ public class MergeSort implements Sort {
             temp[index++] = arr[l++];
         }
 
+        // 임시 배열 temp에 저장한 값을 원본 배열 arr로 복사한다.
         for(int i = 0; i < N; i++) {
             arr[left + i] = temp[i];
         }
