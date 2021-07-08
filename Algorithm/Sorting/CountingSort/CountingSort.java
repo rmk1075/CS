f7bc94f25161d1e45f76367bc720e62a1af50988
jamie <rmk1075@gmail.com>
2021-07-09 01:26:22 +0900

    comment: Counting Sort 주석 추가

--- Algorithm/Sorting/CountingSort/CountingSort.java
+++ Algorithm/Sorting/CountingSort/CountingSort.java
@@ -10,6 +10,7 @@ public class CountingSort implements Sort {
 
     private int N;
 
+    // N은 배열에 들어가는 원소의 값의 범위이다.
     public CountingSort(int[] array, int N) {
         this.array = Arrays.copyOf(array, array.length);
         this.N = N;
@@ -17,11 +18,13 @@ public class CountingSort implements Sort {
 
     @Override
     public int[] sort() {
+        // array에 저장된 원소들의 값을 계수한다.
         int[] countingArray = new int[N + 1];
         for(int num : array) {
             countingArray[num]++;
         }
 
+        // 임시 배열에 계수된 만큼 원소들을 차례로 결과 배열에 입력한다.
         int index = 0;
         for(int i = 0; i < N + 1; i++) {
             while(0 < countingArray[i]--) {
