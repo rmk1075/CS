274989ba3ede2e42fe50effdc879ae909e40026e
jamie <rmk1075@gmail.com>
2021-09-28 01:10:11 +0900

    feat: add linkedlist and heap for Prim Algorithm

--- Algorithm/Graph/Prim/Prim.java
+++ Algorithm/Graph/Prim/Prim.java
@@ -1,8 +1,29 @@
 package Algorithm.Graph.Prim;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
 
 import Algorithm.Graph.Graph;
+
+class Node {
+    int vertex;
+    int weight;
+
+    public Node(int vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+}
+
+class NodeComparator implements Comparator<Node> {
+    @Override
+    public int compare(Node o1, Node o2) {
+        return Integer.compare(o1.weight, o2.weight);
+    }
+}
 
 public class Prim implements Graph {
 
@@ -74,9 +95,59 @@ public class Prim implements Graph {
         return result;
     }
 
+    private int[][] primList() {
+        List<Node>[] graphLists = new LinkedList[N];
+        for(int i = 0; i < N; i++) {
+            graphLists[i] = new LinkedList<>();
+            for(int j = 0; j < N; j++) {
+                if(this.graph[i][j] == Integer.MAX_VALUE) continue;
+                graphLists[i].add(new Node(j, this.graph[i][j]));
+            }
+        }
+
+        int[][] result = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = i + 1; j < N; j++) {
+                result[i][j] = result[j][i] = -1;
+            }
+        }
+
+        Node[] v = new Node[N];
+        int[] parent = new int[N];
+        for(int i = 0; i < N; i++) {
+            v[i] = new Node(i, Integer.MAX_VALUE);
+            parent[i] = -1;
+        }
+
+        v[0].weight = 0;
+        parent[0] = 0;
+        visited[0] = true;
+
+        TreeSet<Node> queue = new TreeSet<Node>(new NodeComparator());
+        for(int i = 0; i < N; i++) queue.add(v[i]);
+
+        while(!queue.isEmpty()) {
+            Node node = queue.pollFirst();
+            visited[node.vertex] = true;
+            result[parent[node.vertex]][node.vertex] = result[node.vertex][parent[node.vertex]] = v[node.vertex].weight;
+            this.weight += v[node.vertex].weight;
+
+            for(Node next : graphLists[node.vertex]) {
+                if(visited[next.vertex] || v[next.vertex].weight <= next.weight) continue;
+                queue.remove(v[next.vertex]);
+                v[next.vertex].weight = next.weight;
+                queue.add(v[next.vertex]);
+                parent[next.vertex] = node.vertex;
+            }
+        }
+
+        return result;
+    }
+
     @Override
     public void search() {
-        graph = prim();
+        // graph = prim();
+        graph = primList();
 
         System.out.println("[Prim]");
         System.out.println("weight of MST: " + weight);
