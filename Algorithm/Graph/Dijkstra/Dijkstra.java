6f77a11583576c80cbabbe24f1c667343091a6b0
jamie <rmk1075@gmail.com>
2021-09-27 20:05:27 +0900

    comment: add comment for Dijkstra's algorithm

--- Algorithm/Graph/Dijkstra/Dijkstra.java
+++ Algorithm/Graph/Dijkstra/Dijkstra.java
@@ -27,21 +27,24 @@ public class Dijkstra implements Graph {
         pq = new PriorityQueue<>();
     }
 
+    // src: 출발점의 인덱스
     private int[] dijkstra(int src) {
-        int[] distance = new int[N];
+        int[] distance = new int[N]; // src로부터 다른 정점들까지의 최단 거리를 저장하는 배열
         for(int i = 0; i < N; i++) {
             if(i == src) continue;
             distance[i] = Integer.MAX_VALUE;
             pq.add(new Node(i, distance[i]));
         }
         distance[src] = 0;
-        pq.add(new Node(src, 0));
+        pq.add(new Node(src, 0));   // Node(정점의 인덱스, 정점의 최단거리)
 
         while(!pq.isEmpty()) {
+            // priority queue에서 현재 최단 거리를 가지고 있는 정점을 구한다.
             Node node = pq.poll();
             if(visited[node.index])
                 continue;
             
+            // 미방문 정점인 경우 방문으로 바꾼 후 해당 정점과 인접한 정점들에 대해 거리를 계산, 비교한다.
             visited[node.index] = true;
             for(int i = 0; i < N; i++) {
                 if(visited[i] || distance[i] < distance[node.index] + graph[node.index][i])
