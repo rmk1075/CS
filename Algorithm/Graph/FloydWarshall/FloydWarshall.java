ec1027502348903980176a1bdaec8dd31003e7fc
jamie <rmk1075@gmail.com>
2021-09-27 22:46:21 +0900

    comment: add comment for floydWarshall algorithm

--- Algorithm/Graph/FloydWarshall/FloydWarshall.java
+++ Algorithm/Graph/FloydWarshall/FloydWarshall.java
@@ -21,9 +21,12 @@ public class FloydWarshall implements Graph {
     }
 
     private void floydWarshall() {
+        // 모든 정점이 한번씩 중간 경로가 되게 한다.
         for(int k = 0; k < N; k++) {
+            // 그래프 내에서 될 수 있는 모든 정점 쌍 (i, j)를 구한다.
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < N; j++) {
+                    // 현재 (i, j)의 최단 거리와 k를 중간경로로 한 거리를 비교한다.
                     if(graph[i][j] < graph[i][k] + graph[k][j]) continue;
                     graph[i][j] = graph[i][k] + graph[k][j];
                 }
